1abde076f3ff32b5e443df9ea28210c4e7508855
MikeHo <jinkind_ho@mymail.sutd.edu.sg>
2019-09-09 10:56:25 +0800

    done

--- src/BasicUtils.java
+++ src/BasicUtils.java
@@ -1,32 +1,94 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 public class BasicUtils {
-    public void login(WebDriver driver){
-        WebElement username = driver.findElement(By.id("userid"));
-        username.sendKeys("1002846");
-        WebElement password = driver.findElement(By.id("pwd"));
-        password.sendKeys("Sutd1234");
+
+    public String classSearchURL = "https://myportal.sutd.edu.sg/psp/EPPRD/EMPLOYEE/HRMS/c/SA_LEARNER_SERVICES.CLASS_SEARCH.GBL?FolderPath=PORTAL_ROOT_OBJECT.CO_EMPLOYEE_SELF_SERVICE.HCCC_SS_CATALOG.HC_CLASS_SEARCH&IsFolder=false&IgnoreParamTempl=FolderPath%2cIsFolder";
+    public String addClassURL = "https://myportal.sutd.edu.sg/psp/EPPRD/EMPLOYEE/HRMS/c/SA_LEARNER_SERVICES.SSR_SSENRL_CART.GBL?FolderPath=PORTAL_ROOT_OBJECT.CO_EMPLOYEE_SELF_SERVICE.HCCC_ENROLLMENT.HC_SSR_SSENRL_CART_GBL&IsFolder=false&IgnoreParamTempl=FolderPath%2cIsFolder";
+    public  String commonFrameID = "ptifrmtgtframe";
+
+    public void login(WebDriver driver, String username, String password){
+        WebElement usernameElement = driver.findElement(By.id("userid"));
+        usernameElement.sendKeys(username.toString());
+        WebElement passwordElement = driver.findElement(By.id("pwd"));
+        passwordElement.sendKeys(password.toString());
         driver.findElement(By.name("Submit")).click();
     }
 
+
     public Boolean checkClassesReleased(WebDriver driver){
-        driver.switchTo().frame("ptifrmtgtframe");
-        WebElement checker = driver.findElement(By.id("DERIVED_REGFRM1_SS_MESSAGE_LONG"));
-        if (!checker.getText().equals("You do not have access to enrollment at this time.")){
+        navigateIframe(commonFrameID, driver);
+        if(!driver.findElements(By.id("DERIVED_REGFRM1_SS_MESSAGE_LONG")).isEmpty()){
+            WebElement checker = driver.findElement(By.id("DERIVED_REGFRM1_SS_MESSAGE_LONG"));
+            if (!checker.getText().equals("You do not have access to enrollment at this time.")){
 
 
-            System.out.println(checker.getText());
-            System.out.println(checker.getText() != "You do not have access to enrollment at this time.");
-            return false;
+                System.out.println(checker.getText());
+                System.out.println(checker.getText() != "You do not have access to enrollment at this time.");
+                return false;
+            }
+            else {
+                driver.navigate().refresh();
+                System.out.println("refresh");
+                return true;
+
+            }
         }
-        else{
-            driver.navigate().refresh();
-            System.out.println("refresh");
-            return true;
+        return true;
+    }
 
+    public void selectSearchCriteriaPage_stepThree(WebDriver driver, String session){
+        navigateIframe(commonFrameID, driver);
+        //Select termDropDown = new Select(driver.findElement(By.id("CLASS_SRCH_WRK2_STRM$35$")));
+        //termDropDown.selectByVisibleText("Acad Year 2019 Sep");
+        //select subject
+        Select subjectDropDown = new Select(driver.findElement(By.id("SSR_CLSRCH_WRK_SUBJECT_SRCH$0")));
+        subjectDropDown.selectByVisibleText(session.substring(0,2));
+        Select courseDropDown = new Select(driver.findElement(By.id("SSR_CLSRCH_WRK_SSR_EXACT_MATCH1$1")));
+        courseDropDown.selectByVisibleText("contains");
+        WebElement password = driver.findElement(By.id("SSR_CLSRCH_WRK_CATALOG_NBR$1"));
+        password.sendKeys(session.substring(2,6));
+
+        driver.findElement(By.name("CLASS_SRCH_WRK2_SSR_PB_CLASS_SRCH")).click();
+    }
+    public void navigateIframe(String iframe, WebDriver driver){
+        driver.switchTo().defaultContent();
+        if(!driver.findElements(By.id(iframe)).isEmpty()){
+            driver.switchTo().frame(iframe);
         }
     }
+
+    public void selectClassSearchDummyPage_stepOne(WebDriver driver){
+        //TODO click radio button of term, and continue button
+        driver.findElement(By.id("DERIVED_REGFRM1_SSR_PB_SRCH")).click();
+    }
+
+    public void selectClassTimingPage_step_four(WebDriver driver){
+        //TODO need to click the right timing  SSR_PB_SELECT$0, SSR_PB_SELECT$1, depending on class
+        navigateIframe(commonFrameID, driver);
+        driver.findElement(By.id("win0divSSR_PB_SELECT$0")).click();
+    }
+
+    public void firstConfirmationPage_stepFive(WebDriver driver){
+        //TODO click next
+        navigateIframe(commonFrameID, driver);
+        driver.findElement(By.id("win0divDERIVED_CLS_DTL_NEXT_PB$280$")).click();
+
+    }
+
+    public void secondConfirmationPage_stepSeven(WebDriver driver){
+        //TODO click proceed to step 2 of 3 DERIVED_REGFRM1_LINK_ADD_ENRL$82$
+        navigateIframe(commonFrameID, driver);
+        driver.findElement(By.id("win0divDERIVED_REGFRM1_LINK_ADD_ENRL$82$")).click();
+    }
+
+    public void finishEnrollingClassPage(WebDriver driver){
+        navigateIframe(commonFrameID, driver);
+        driver.findElement(By.id("win0divDERIVED_REGFRM1_SSR_PB_SUBMIT")).click();
+    }
+    //after this we can add multiple classes, or we can immediately finish enrolling
+    // for now we will just finish enrolling   win0divDERIVED_REGFRM1_SSR_PB_SUBMIT
 
 }
--- src/ClassPicker.java
+++ src/ClassPicker.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.lang.reflect.Array;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -6,13 +7,19 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+//things to improve: crash if class not found
+//select timing of class, currently, always select first one
 public class ClassPicker {
 
     public static void main(String[] args) throws IOException, InterruptedException {
+        String username = "1002846";
+        String password = "Sutd1234";
+        //note, sort your mods, by most impt, if classes run out, it will crash
+        String[] sessions = {"02.139","50.042"};
         System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--no-sandbox");
-        Thread.sleep(5000);
+        Thread.sleep(1000);
 
         WebDriver driver = new ChromeDriver(chromeOptions);
 
@@ -21,13 +28,30 @@ public class ClassPicker {
         Thread.sleep(1000);
         try {
             BasicUtils utils = new BasicUtils();
-            utils.login(driver);
-            driver.navigate().to("https://myportal.sutd.edu.sg/psp/EPPRD/EMPLOYEE/HRMS/c/SA_LEARNER_SERVICES.SSR_SSENRL_CART.GBL?FolderPath=PORTAL_ROOT_OBJECT.CO_EMPLOYEE_SELF_SERVICE.HCCC_ENROLLMENT.HC_SSR_SSENRL_CART_GBL&IsFolder=false&IgnoreParamTempl=FolderPath%2cIsFolder");
-            Thread.sleep(1000);
-            Boolean refresh = true;
-            while (refresh) {
-                refresh = utils.checkClassesReleased(driver);
+            utils.login(driver,  username, password);
+            //for loop here
+            for(int i=0;i<sessions.length;i++){
+                driver.navigate().to(utils.addClassURL);
                 Thread.sleep(1000);
+                Boolean classAvailable = false;
+                while (!classAvailable) {
+                    classAvailable = utils.checkClassesReleased(driver);
+                    Thread.sleep(1000);
+                }
+                System.out.println("found");
+                    utils.selectClassSearchDummyPage_stepOne(driver);
+                    Thread.sleep(1000);
+                    utils.selectSearchCriteriaPage_stepThree(driver, sessions[i]);
+                    Thread.sleep(1000);
+                    utils.selectClassTimingPage_step_four(driver);
+                    Thread.sleep(1000);
+                    utils.firstConfirmationPage_stepFive(driver);
+                    Thread.sleep(1000);
+                    utils.secondConfirmationPage_stepSeven(driver);
+                    Thread.sleep(1000);
+                    //warning: this will enroll you into class, only use at production phase, not development
+                    //utils.finishEnrollingClassPage(driver);
+                    Thread.sleep(5000);
             }
         } catch (Exception e) {
             System.out.println(e);
